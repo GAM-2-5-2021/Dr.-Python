2845c280642f1ab605bbe71688ced741087cc6c5
vlah332 <83946125+vlah332@users.noreply.github.com>
2021-06-09 17:48:07 +0200

    Add files via upload

---
+++ information.py
@@ -0,0 +1,6 @@
+Prednji_dio="\nM. deltoideus,\nM. pectoralis major,\nlinea alba,\nM. serratus anterior,\nM. rectus abdominis,\nM. obliquus externus abdominis,\nSpina iliaca anterior superior"
+def main():
+    pass
+if __name__ == "__main__":
+    main()
+
